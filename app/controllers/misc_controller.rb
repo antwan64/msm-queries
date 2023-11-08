28ccc8a3ba342a084bd385e03995629e97912def
antwan64 <146212683+antwan64@users.noreply.github.com>
2023-11-08 15:51:28 +0000

    Entering Boiler Plate

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -2,4 +2,25 @@ class MiscController < ApplicationController
   def homepage
     render({ :template => "misc_templates/home"})
   end
+
+  def directors
+    render({ :template => "misc_templates/directors"})
+  end
+
+  def eldest
+    render({ :template => "misc_templates/eldest"})
+  end
+
+  def youngest
+    render({ :template => "misc_templates/youngest"})
+  end
+
+  def actors
+    render({ :template => "misc_templates/actors"})
+  end
+
+  def movies
+    render({ :template => "misc_templates/movies"})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
+
+  get("/directors", { :controller => "misc", :action => "directors" })
+
+  get("/directors/yougest", { :controller => "misc", :action => "youngest" })
+
+  get("/directors/eldest", { :controller => "misc", :action => "eldest" })
+
+  get("/movies", { :controller => "misc", :action => "movies" })
+
+  get("/actors", { :controller => "misc", :action => "actors" })
 end
