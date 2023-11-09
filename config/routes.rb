1cb68e04f0e161439d9093f9c54a80be0d68f517
antwan64 <146212683+antwan64@users.noreply.github.com>
2023-11-09 18:21:36 +0000

    Director List complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
 
-  get("/directors", { :controller => "directors", :action => "all_directors" })
+  get("/directors", { :controller => "directors", :action => "index" })
 
   get("/directors/yougest", { :controller => "directors", :action => "youngest" })
 
